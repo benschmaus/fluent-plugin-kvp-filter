8f1123bcd58962b5a45b8f46fffc560ff9e6f7d4
Matt DeBoer <matt.deboer@gettyimages.com>
2017-02-18 23:31:31 -0800

    convert to a filter; add 'remove_parse_key' config

--- lib/fluent/plugin/out_fields_parser.rb
+++ lib/fluent/plugin/out_fields_parser.rb
@@ -1,44 +1,24 @@
+require 'fluent/time'
+require 'fluent/config/error'
+require 'fluent/plugin/filter'
 require "logfmt"
 
-module Fluent
-  class OutputFieldsParser < Fluent::Output
-    Fluent::Plugin.register_output('fields_parser', self)
+module Fluent::Plugin
+  class ParserFilter < Filter
+    Fluent::Plugin.register_filter('kvp', self)
 
-    config_param :remove_tag_prefix,  :string, :default => nil
-    config_param :add_tag_prefix,     :string, :default => nil
     config_param :parse_key,          :string, :default => 'message'
     config_param :fields_key,         :string, :default => nil
     config_param :pattern,            :string,
                  :default => %{([a-zA-Z_]\\w*)=((['"]).*?(\\3)|[\\w.@$%/+-]*)}
     config_param :strict_key_value,  :bool, :default => false
+    config_param :remove_parse_key,  :bool, :default => false
 
-    def compiled_pattern
-      @compiled_pattern ||= Regexp.new(pattern)
+    def configure(conf)
+      super
     end
 
-    def emit(tag, es, chain)
-      tag = update_tag(tag)
-      es.each { |time, record|
-        router.emit(tag, time, parse_fields(record))
-      }
-      chain.next
-    end
-
-    def update_tag(tag)
-      if remove_tag_prefix
-        if remove_tag_prefix == tag
-          tag = ''
-        elsif tag.to_s.start_with?(remove_tag_prefix+'.')
-          tag = tag[remove_tag_prefix.length+1 .. -1]
-        end
-      end
-      if add_tag_prefix
-        tag = tag && tag.length > 0 ? "#{add_tag_prefix}.#{tag}" : add_tag_prefix
-      end
-      return tag
-    end
-
-    def parse_fields(record)
+    def filter(tag, time, record)
       source = record[parse_key].to_s
       target = fields_key ? (record[fields_key] ||= {}) : record
 
@@ -56,6 +36,10 @@ module Fluent
           to_pos = value.length - ending_quote.to_s.length - 1
           target[key] = value[from_pos..to_pos]
         end
+      end
+
+      if remove_parse_key
+        record.delete parse_key
       end
 
       return record
